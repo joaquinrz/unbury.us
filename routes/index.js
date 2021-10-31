2d81ff871d42f52207670950e3d1072b21497c8d
Joaquin Rodriguez <rjoaquin@microsoft.com>
2021-10-31 09:56:51 -0500

    Removed opportunity_cost handle

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,6 @@ router.get('/', function(req, res, next) {
   res.render("index");
 });
 
-router.get('/opportunity_cost', function(req, res, next) {
-    res.render("opportunity_cost");
-});
-
 router.get('/fi_calculator', function(req, res, next) {
     res.render("fi_calculator");
 });
