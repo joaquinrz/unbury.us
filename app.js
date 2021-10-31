40fc559c2ae4da6faea45212a937ad7335408b58
Joaquin Rodriguez <rjoaquin@microsoft.com>
2021-10-31 09:56:30 -0500

    Added read BASE_URL from environment

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+var baseUrl = process.env.BASE_URL || '/';
+
+app.use(baseUrl, routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
